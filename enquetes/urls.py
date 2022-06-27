3c0be5756854ec5a5807bd256cfb9d62631ef7b2
Raphael Matias <raphaelmatias135790@gmail.com>
2022-06-26 23:15:22 -0300

    Adicionando funcionalidades de votação e de ver resultados parciais

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -1,6 +1,25 @@
 from django.urls import path
-from .views import index
+from . import views
 
+app_name = 'enquetes'
 urlpatterns = [
-    path('', index),
+    path('', views.index, name='index'),
+
+    path(
+        'enquetes/<int:question_id>/',
+        views.details, name='detail'
+    ),
+
+    # ex.: resultado - /enquete/5/resultado/
+    path(
+        'enquetes/<int:question_id>/results/',
+        views.results, name='results'
+    ),
+
+    # ex.: votação - /enquete/5/votacao/
+    path(
+        'enquetes/<int:question_id>/votacao/',
+        views.vote, name='votacao'
+    ),
+
 ]
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
+from .models import Choice, Question
 
 def index(request):
-    return render(request, 'index.html')
+    latest_question_list = Question.objects.order_by('pub_date') [:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
 
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'details.html', {'question':question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'details.html',
+        {'question': question,
+         'error_message': "Você não selecionou uma opção"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('enquetes:results', args=(question_id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question':question})
